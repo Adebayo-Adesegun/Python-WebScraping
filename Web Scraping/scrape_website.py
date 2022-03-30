825bafe7a6d308c8430dc19262dd00776d7aa2c3
daniel-adesegun <100849011+daniel-adesegun@users.noreply.github.com>
2022-03-30 21:50:12 +0100

    adjust and add support for multiple pages

--- Web Scraping/scrape_website.py
+++ Web Scraping/scrape_website.py
@@ -16,19 +16,26 @@ def in_stock(title, topic):
         if topic_url is None:
             return False
 
-        status, topic_page = get_page(base_url + topic_url)
 
-        if status is False:
-            return False
+        # Assume they are multiple pages
+        for elem in range(10):
+            if elem == 0:
+                url_inner = base_url + topic_url
+            else:
+                url_inner = base_url + topic_url.replace('/index.html', f'/page-{elem + 1}.html')
 
-        body = get_page_content(topic_page, 'row', 'ol')
+            status, topic_page = get_page(url_inner)
 
-        for elem in body[0].find_all('li'):
-            elem_title = elem.find_all('h3')[0].get_text().strip().lower()
-            elem_title = re.sub('[^A-Za-z0-9]+', ' ', elem_title)
+            if status is False:
+                return False
 
-            if elem_title == title.lower():
-                return True
+            body = get_page_content(topic_page, 'row', 'ol')
+
+            for elem in body[0].find_all('li'):
+                elem_title = elem.find_all('h3')[0].find_all('a')[0].get('title').strip().lower()
+                # elem_title = re.sub('[^A-Za-z0-9]+', ' ', elem_title)
+                if elem_title == title.lower():
+                    return True
         return False
     else:
         return False
@@ -50,14 +57,13 @@ def get_page(url):
 
 
 def get_page_content(page_content, class_name, html_tag_name='div'):
-    soup = BeautifulSoup(page_content, 'html5lib')
+    soup = BeautifulSoup(page_content, 'html.parser')
     body = soup.find_all(html_tag_name, class_=class_name)
     return body
 
 
 def get_category_url(page_content, topic):
     body = page_content[0].find_all('a')
-
     for elem in body:
         if elem.get_text().strip().lower() == topic.lower():
             return elem['href']
@@ -65,6 +71,9 @@ def get_category_url(page_content, topic):
     return None
 
 
-print(in_stock("the origin of species", "science"))
-print(in_stock("the origin of species", "art"))
-print(in_stock("Origin of Species", "Science"))
+# print(in_stock("the origin of species", "science"))
+# print(in_stock("the origin of species", "art"))
+# print(in_stock("Origin of Species", "Science"))
+# print(in_stock("While You Were Mine", "Historical Fiction"))
+
+print(in_stock("Online Marketing for Busy Authors: A Step-By-Step guide", "Self help"))
